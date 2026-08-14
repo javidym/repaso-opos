0d6a3bc39e7b8f2cf6b1b77f9a1ef413b9f27228
javidym <javidym@gmail.com>
2026-08-14 15:44:14 +0200

    Numeros por categoria (T47-53): mas volumen (933) y distractores trampa = vecinos cercanos (numeros y elementos contiguos)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker · Repaso OPOS
    Sube el número de versión cuando cambies preguntas.js o el código. */
-var VERSION = 'repaso-opos-v67';
+var VERSION = 'repaso-opos-v68';
 var ASSETS = [
   './',
   './index.html',
